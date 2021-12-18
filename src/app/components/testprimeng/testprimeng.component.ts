bf53c9f60998762e23a8c20177dbf045477cc1a6
MarcosValdez <marcos.valdez333@hotmail.com>
2021-12-18 18:01:41 -0500

    fix: agregando

--- src/app/components/testprimeng/testprimeng.component.ts
+++ src/app/components/testprimeng/testprimeng.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestprimengComponent implements OnInit {
   value: Date;
+  //texto de prueba
   constructor() {
     // this.value = new Date();
   }

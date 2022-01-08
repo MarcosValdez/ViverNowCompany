431de4dbbe59e76098da4ffd058520e065f24c76
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-01-08 15:42:20 -0500

    feat: Implement Auth guards

--- src/app/remodelacion/remodelacion-routing.module.ts
+++ src/app/remodelacion/remodelacion-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { NuevoProyectoComponent } from './pages/nuevo-proyecto/nuevo-proyecto.component';
 import { ProyeccionRentabilidadComponent } from './pages/proyeccion-rentabilidad/proyeccion-rentabilidad.component';
 
 const routes: Routes = [
-  { path: 'nuevo', component: NuevoProyectoComponent },
-  { path: 'rentabilidad', component: ProyeccionRentabilidadComponent },
+  {
+    path: 'nuevo',
+    component: NuevoProyectoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'rentabilidad',
+    component: ProyeccionRentabilidadComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({

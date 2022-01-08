71a272e3cb3c1b49d4a4e094da2060cdcbaaeae4
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-01-07 23:40:07 -0500

    feat: Create remodelacion module

---
+++ src/app/remodelacion/remodelacion.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RemodelacionRoutingModule } from './remodelacion-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RemodelacionRoutingModule],
+})
+export class RemodelacionModule {}

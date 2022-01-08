5e218c7895cafd5106bb830f3796ded66ff12d08
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-01-08 00:04:33 -0500

    feat: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestprimengComponent } from './components/testprimeng/testprimeng.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TestprimengComponent,
+  },
+  {
+    path: 'remodelacion',
+    loadChildren: () =>
+      import('./remodelacion/remodelacion.module').then(
+        (m) => m.RemodelacionModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/remodelacion/remodelacion.module.ts
+++ src/app/remodelacion/remodelacion.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RemodelacionRoutingModule } from './remodelacion-routing.module';
+import { NuevoProyectoComponent } from './pages/nuevo-proyecto/nuevo-proyecto.component';
+import { ProyeccionRentabilidadComponent } from './pages/proyeccion-rentabilidad/proyeccion-rentabilidad.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NuevoProyectoComponent, ProyeccionRentabilidadComponent],
   imports: [CommonModule, RemodelacionRoutingModule],
 })
 export class RemodelacionModule {}

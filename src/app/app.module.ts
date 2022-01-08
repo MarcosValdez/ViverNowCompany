f88bc275acda53ca8c498413ca307774383b5f8b
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-01-08 15:20:40 -0500

    feat: Add Http error interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { TestprimengComponent } from './components/testprimeng/testprimeng.compo
 
 import { CalendarModule } from 'primeng-lts/calendar';
 import { ButtonModule } from 'primeng-lts/button';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpErrorService } from './core/interceptors/http-error.service';
 
 @NgModule({
   declarations: [AppComponent, TestprimengComponent],
@@ -23,7 +24,13 @@ import { HttpClientModule } from '@angular/common/http';
     ButtonModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/core/interceptors/http-error.service.ts
@@ -0,0 +1,32 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable()
+export class HttpErrorService implements HttpInterceptor {
+  constructor() {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.log('Client error', error.error.message);
+    } else {
+      // Error en el lado del servidor
+      console.log('Error Status:', error.status);
+      console.log('Error:', error.error);
+    }
+    //catch and rethrow
+    return throwError('Cannot perform the request, please try again later');
+  }
+}

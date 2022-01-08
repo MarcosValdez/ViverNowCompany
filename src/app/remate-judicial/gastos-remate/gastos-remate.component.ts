ccc8b3f424a54ef0282027af121eeb9d953301e3
MarcosValdez <marcos.valdez333@hotmail.com>
2022-01-08 16:03:58 -0500

    built: configuracion package.json

---
+++ src/app/remate-judicial/gastos-remate/gastos-remate.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gastos-remate',
+  templateUrl: './gastos-remate.component.html',
+  styleUrls: ['./gastos-remate.component.css'],
+})
+export class GastosRemateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
